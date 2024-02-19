d7c2e94e027cee6eb9b44fdf36a22ae62d589e6c
TheSpyGod <gabrielkorczyk196813@outlook.com>
2024-02-19 12:33:01 +0000

    Updated the way it reviews the string; Added multiple opperation capability.

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-void splitString(const char string[]) {
-    int length = strlen(string);
+#include <ctype.h>
+int splitString(const char* string) {
+    /*int length = strlen(string);
     int start = 0;
     int end;
 
@@ -16,60 +16,62 @@ void splitString(const char string[]) {
             putchar('\n');
             start = i + 1;
         }
-    }
-}
+    }*/
+    int result = 0;
+    int number = 0;
+    char opp = '+';
 
-double calc(char symbol, const char *num1, const char *num2) {
-    int number1 = atoi(num1);
-    int number2 = atoi(num2);
-    double result;
-    switch (symbol) {
-        case '+': 
-            result = number1 + number2;
+    for (int i = 0; string[i] != '\0'; i++) {
+      if (isdigit(string[i])) {
+        number = number * 10 + (string[i] - '0');
+      } else if (string[i] == '+' || string[i] == '-' || string[i] == '*' || string[i] == '/') {
+        switch(opp) {
+                case '+':
+                    result += number;
+                    break;
+                case '-':
+                    result -= number;
+                    break;
+                case '*':
+                    result *= number;
+                    break;
+                case '/':
+                    result /= number;
+                    break;
+            }
+        number = 0;
+        opp = string[i];
+      }
+   }
+   switch(opp) {
+        case '+':
+            result += number;
             break;
-        case '-': 
-            result = number1 - number2;
+        case '-':
+            result -= number;
             break;
-        case '*': 
-            result = number1 * number2;
+        case '*':
+            result *= number;
             break;
-        case '/': 
-            result = (double)number1 / number2;
+        case '/':
+            result /= number;
             break;
-        default:
-            printf("Invalid symbol\n");
-            result = 0.0; // Default result in case of invalid symbol
     }
-
+    
     return result;
 }
 
 int main() {
-    char symbol;
     char string[100];
     char choice;
-    char num1[20], num2[20];
 
     printf("\tProject Calculator\n");
  
     do {
-    /*
-    printf("Enter the first number: ");
-    scanf("%s", num1);
-
-    printf("Enter the second number: ");
-    scanf("%s", num2);
-
-    
-     printf("Enter symbol (+, -, *, /): ");
-    scanf(" %c", &symbol); // Note the space before %c to consume any leading whitespace
-    */
     printf("Enter numeric expression: ");
     fgets(string, sizeof(string), stdin);
-    splitString(string);
-    sscanf(string, "%s %c %s", num1, &symbol, num2);
-    double result = calc(symbol, num1, num2);
-    printf("Result: %.2f\n", result);
+    int result = splitString(string);
+    printf("Result: %.2d\n", result);
     printf("Continue? q/Q");
     scanf(" %c", &choice);
     getchar();
